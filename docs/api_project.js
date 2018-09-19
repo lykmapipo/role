e4eefcb2df2b6f6ce576ae1c78d49f16a1e07cab
lykmapipo <lallyelias87@gmail.com>
2018-09-19 17:02:32 +0300

    build api doc

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "role",
+  "version": "1.0.0",
+  "description": "An entity that defines how a party(ies) acts or, in other words, what roles the party(ies) plays in specific context or environment.",
+  "title": "Role",
+  "url": "https://role.herokuapp.com/v1",
+  "sampleUrl": "https://role.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-09-19T14:01:15.077Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
